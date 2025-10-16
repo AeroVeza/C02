fe10d1304b9201950357455d0a1483c8f1f1a029
Your Name <you@example.com>
2025-10-16 11:39:52 +0200

    adding files

--- ex00/main.c
+++ ex00/main.c
@@ -1,1 +1,16 @@
-test
+#include <stdio.h>
+
+int	ipssi_strncmp(char *s1, char *s2, unsigned int n);
+
+int	main(void)
+{
+	char s1[] = "";
+	char s2[] = "";
+	unsigned int n = 7;
+	int result;
+
+	result = ipssi_strncmp(s1, s2, n);
+	printf("%d\n", result);
+
+	return (0);
+}
